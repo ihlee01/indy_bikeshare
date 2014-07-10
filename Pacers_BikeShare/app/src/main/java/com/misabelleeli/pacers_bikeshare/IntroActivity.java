c03d04195914b067d75aaf5debdda9db96b036be
Inhwan Lee <ihlee01@hotmail.com>
2014-07-10 00:03:49 -0400

    Adjust logo

--- Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/IntroActivity.java
+++ Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/IntroActivity.java
@@ -17,7 +17,7 @@ public class IntroActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Full page logo screen
+        // Full page logo screen - remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_intro);
 
