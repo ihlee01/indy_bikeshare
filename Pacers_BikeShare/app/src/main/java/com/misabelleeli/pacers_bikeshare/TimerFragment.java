8efcef1280bd1fed81e5287a00871070559423a7
Isabel Lee <m.isabellee@hotmail.com>
2014-07-11 00:49:18 -0400

    CountDown Timer implemented.
    Notification is partially working. Trying to find out how to get the
    text showing up with the timer. Also, notification will vibrate for at
    least 1 minute as of right now.

--- Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/TimerFragment.java
+++ Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/TimerFragment.java
@@ -1,16 +1,13 @@
 package com.misabelleeli.pacers_bikeshare;
 
-
-
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
@@ -20,8 +17,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RemoteViews;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -31,16 +29,13 @@ import android.widget.TextView;
 public class TimerFragment extends Fragment {
 
     private Button startButton;
-    private Button pauseButton;
     private Button resetButton;
     private TextView timerValue;
-    private long startTime = 0;
-    private Handler customHandler = new Handler();
-
-    long timeInMilliseconds = 0;
-    long timeSwapBuff = 0;
-    long updatedTime = 0;
-    long five_minutes = 500; //300000;
+    private long startTime = 1800000; //milliseconds
+    private String hms = "";
+    private long delimiter = 28;
+    final CounterClass timer = new CounterClass(startTime,1000);
+    private NotificationCompat.Builder mBuilder;
 
     public TimerFragment() {
         // Required empty public constructor
@@ -69,103 +64,93 @@ public class TimerFragment extends Fragment {
         timerValue = (TextView) getView().findViewById(R.id.timerValue);
         startButton = (Button) getView().findViewById(R.id.startbutton);
         startButton.setOnClickListener(handler);
-/*
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startTime = SystemClock.uptimeMillis();
-                customHandler.postDelayed(updateTimerThread,0);
-            }
-        });
-*/
-        pauseButton = (Button) getView().findViewById(R.id.pause);
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timeSwapBuff += timeInMilliseconds;
-                customHandler.removeCallbacks(updateTimerThread);
-            }
-        });
+
 
         resetButton = (Button) getView().findViewById(R.id.reset);
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                customHandler.removeCallbacks(updateTimerThread);
-                timeInMilliseconds = 0;
-                timeSwapBuff = 0;
-                updatedTime = 0;
+                timer.cancel();
                 timerValue.setText("00:00:00");
             }
         });
     }
 
     private void showNotification(){
-        startTime = SystemClock.uptimeMillis();
-        customHandler.postDelayed(updateTimerThread,0);
+        timer.start();
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-                getActivity()).setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("Rescue Me ALARM")
-                .setContentText("Press here to cancel the SOS SMS");
+        mBuilder = new NotificationCompat.Builder(
+                getActivity()).setSmallIcon(R.drawable.ic_launcher);
+        mBuilder.setContentTitle("Pacers Bike Share Timer");
 
-        // Make the notification play the default notification sound:
-        Uri alarmSound = RingtoneManager
-                .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        mBuilder.setSound(alarmSound);
-        mBuilder.setOngoing(true);
-        mBuilder.setAutoCancel(true);
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(getActivity(), MainActivity.class);
-
-        // This somehow makes sure, there is only 1 CountDownTimer going if the notification is pressed:
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        // The stack builder object will contain an artificial back stack for the started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getActivity());
-
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-
-        // Make this unique ID to make sure there is not generated just a brand new intent with new extra values:
-        int requestID = (int) System.currentTimeMillis();
-
-        // Pass the unique ID to the resultPendingIntent:
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(getActivity(), requestID, resultIntent, 0);
-
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager = (NotificationManager) getActivity()
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(0, mBuilder.build());
     }
 
-    private Runnable updateTimerThread = new Runnable(){
-        public void run()
+
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long milliseconds, long countDownInterval)
         {
-            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-            updatedTime = timeSwapBuff + timeInMilliseconds;
-
-            int secs = (int)(updatedTime/1000);
-            int mins = secs/60;
-            secs = secs % 60;
-            int milliseconds = (int) (updatedTime%1000);
-            timerValue.setText(""+mins+":"+String.format("%02d",secs)+":"
-                    + String.format("%02d",milliseconds));
-
-            if(secs == 20)
-            {
-                //Intent intentVibrate =new Intent(getApplicationContext(),VibrateService.class);
-                //startService(intentVibrate);
-            }
-            customHandler.postDelayed(this,0);
-
+            super(milliseconds,countDownInterval);
         }
-    };
+
+        @Override
+        public void onTick(long l) {
+            long millisec = l;
+            long temp = TimeUnit.MILLISECONDS.toMinutes(millisec) -
+                    TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisec));
+            hms = String.format("%02d:%02d:%02d",
+                    TimeUnit.MILLISECONDS.toHours(millisec),
+                    TimeUnit.MILLISECONDS.toMinutes(millisec) -
+                            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisec)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisec) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisec)));
+            timerValue.setText(hms);
+            if(delimiter == temp || 0 == temp)
+            {
+                // Make the notification play the default notification sound:
+                Uri alarmSound = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                mBuilder.setSound(alarmSound);
+                mBuilder.setOngoing(true);
+                mBuilder.setAutoCancel(true);
+                // Creates an explicit intent for an Activity in your app
+                Intent resultIntent = new Intent(getActivity(), MainActivity.class);
+
+                // This somehow makes sure, there is only 1 CountDownTimer going if the notification is pressed:
+                resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                // The stack builder object will contain an artificial back stack for the started Activity.
+                // This ensures that navigating backward from the Activity leads out of
+                // your application to the Home screen.
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(getActivity());
+
+                // Adds the back stack for the Intent (but not the Intent itself)
+                stackBuilder.addParentStack(MainActivity.class);
+
+                // Adds the Intent that starts the Activity to the top of the stack
+                stackBuilder.addNextIntent(resultIntent);
+
+                // Make this unique ID to make sure there is not generated just a brand new intent with new extra values:
+                int requestID = (int) System.currentTimeMillis();
+
+                // Pass the unique ID to the resultPendingIntent:
+                PendingIntent resultPendingIntent = PendingIntent.getActivity(getActivity(), requestID, resultIntent, 0);
+
+                mBuilder.setContentIntent(resultPendingIntent);
+                NotificationManager mNotificationManager = (NotificationManager) getActivity()
+                        .getSystemService(Context.NOTIFICATION_SERVICE);
+
+                // mId allows you to update the notification later on.
+                mNotificationManager.notify(0, mBuilder.build());
+                mBuilder.setContentText("There is " + temp + " minute left!");
+                //Cancel Timer
+                timer.cancel();
+            }
+        }
+
+        @Override
+        public void onFinish() {
+            timerValue.setText("00:00:00");
+        }
+    }
 }
