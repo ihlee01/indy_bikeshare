e61ffa859be06341c1678eb29b4922ba7de261cf
Isabel Lee <m.isabellee@hotmail.com>
2014-08-23 17:35:21 -0400

    Cleaned some code
    Still having issues.

--- Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/GoogleMapFragment.java
+++ Pacers_BikeShare/app/src/main/java/com/misabelleeli/pacers_bikeshare/GoogleMapFragment.java
@@ -90,15 +90,15 @@ public class GoogleMapFragment extends SupportMapFragment implements LocationLis
     }
 
     private class JSONParser extends AsyncTask<String, String, JSONObject> {
-
+        String url = "https://publicapi.bcycle.com" +
+                "/api/1.0/ListProgramKiosks/75";
         String apiKey = "CDF97241-9E01-4C18-AEA9-1DBA42651EA8";
         JSONArray user = null;
 
         @Override
         protected JSONObject doInBackground(String... strings) {
-            String url = "https://publicapi.bcycle.com" +
-                    "/api/1.0/ListProgramKiosks/75";
-            JSONParser jParser = new JSONParser();
+            com.misabelleeli.pacers_bikeshare.JSONParser jParser =
+                    new com.misabelleeli.pacers_bikeshare.JSONParser();
             JSONObject json = jParser.getJSONFromUrl(url);
             return json;
         }
@@ -113,47 +113,6 @@ public class GoogleMapFragment extends SupportMapFragment implements LocationLis
             {
                 e.printStackTrace();
             }
-        }
-        public JSONObject getJSONFromUrl(String url) {
-            InputStream is = null;
-            JSONObject jObj = null;
-            String json = "";
-            // Making HTTP request
-            try {
-                // defaultHttpClient
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpPost httpPost = new HttpPost(url);
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        is, "iso-8859-1"), 8);
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line + "n");
-                }
-                is.close();
-                json = sb.toString();
-            } catch (Exception e) {
-                Log.e("Buffer Error", "Error converting result " + e.toString());
-            }
-            // try parse the string to a JSON object
-            try {
-                jObj = new JSONObject(json);
-            } catch (JSONException e) {
-                Log.e("JSON Parser", "Error parsing data " + e.toString());
-            }
-            // return JSON String
-            return jObj;
         }
 
     }
